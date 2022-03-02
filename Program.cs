d45cba5b1ca40c56db0cebcb87d0df859ec17724
Lamec Vasconcelos <josuelamec@hotmail.com>
2022-03-02 20:46:04 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GFT.src.Entities;
+
+namespace GFT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizzard");
+
+            Console.WriteLine(arus);
+            Console.WriteLine(wizard);
+
+        }
+    }
+}
